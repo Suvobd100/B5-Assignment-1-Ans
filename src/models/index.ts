2bf6fb1b988f6f3f4592064fe2fcd54f735b9aff
suvobd100 <makbarbd@gmail.com>
2025-05-08 19:41:30 +0600

    check with index and make sl

--- src/models/index.ts
+++ src/models/index.ts
@@ -7,28 +7,43 @@ import { getMostExpensiveProduct } from "./6";
 import { Day, getDayType } from "./7";
 import { squareAsync } from "./8";
 
-// 1 const res = formatString('dhaka', true)
-// 2 const res = filterByRating([
+// 1 
+// const res = formatString('bangladesh',true)
+// console.log(res);
+//-------------------##----------
+// 2 
+// const res = filterByRating([
 //     { title: "Book A", rating: 4.5 },
 //     { title: "Book B", rating: 3.2 },
 //     { title: "Book C", rating: 5.0 }
 //   ])
+//   console.log(res);
+// ---------------###---------------------
 
-// 3 const res= concatenateArrays([1, 2], [3, 4], [5])
+// 3 
+// const res= concatenateArrays([1, 2], [3, 4], [5])
+// const res= concatenateArrays(["a", "b"], ["c"],['d'],['3','8','9'])
 // console.log(res);
+
+//------------------------######-----------------------------------
 
 //4 Example usage
 // const myCar = new Car("Toyota", 2020, "Corolla");
 
 // console.log(myCar.getInfo());
 // console.log(myCar.getModel());
+// console.log(myCar);
+
+//------------------------##--------------##---------------------
 
 
 // 5 example
 
-// const res= processValue(2)
+// const res= processValue(10)
 // const res= processValue('hello')
 // console.log(res);
+
+// ---------------------------#######---------------------
 
 // 6 example
 // const products = [
@@ -41,14 +56,17 @@ import { squareAsync } from "./8";
 //   // Output: { name: "Bag", price: 50 }.
 //   console.log(res);
 
+// ---------------------------###---------------##------------------
 
 // 7 example
 // const res1= getDayType(Day.Monday)
 // const res2= getDayType(Day.Sunday)
 // console.log(res1,res2);
 
+// -------------------###-------------------------------------------------
+
 // 8 example
 // const result = squareAsync(-3)
-const result = squareAsync(4)
-.then(result => console.log(result))  // Output after 1s: 16
-    .catch(err => console.error(err));
+// const result = squareAsync(4)
+// .then(result => console.log(result))  // Output after 1s: 16
+//     .catch(err => console.error(err));
